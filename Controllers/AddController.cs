af8ca078a3367706a109743f24cab37711472bee
LucazVaz <lucasvazdev@gmail.com>
2025-06-25 02:08:10 -0300

    Controller and view for sending .ofx files, but not processing them yet.

---
+++ Controllers/AddController.cs
@@ -0,0 +1,61 @@
+﻿using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PFO_Web.Controllers
+{
+    public class AddController : Controller
+    {
+        [HttpGet]
+        public IActionResult Send()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadOfx(IFormFile ofxFile)
+        {
+            if (ofxFile == null || ofxFile.Length == 0)
+                return BadRequest("No file uploaded.");
+
+            using (var reader = new StreamReader(ofxFile.OpenReadStream()))
+            {
+                string? line;
+                var xmlLines = new List<string>();
+                bool ofxStarted = false;
+
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+                    if (ofxStarted)
+                    {
+                        xmlLines.Add(line);
+                    }
+                    else if (line.TrimStart().StartsWith("<OFX>"))
+                    {
+                        ofxStarted = true;
+                        xmlLines.Add(line);
+                    }
+                }
+
+                if (!ofxStarted)
+                    return BadRequest("No <OFX> tag found in file.");
+
+                var xmlContent = string.Join(Environment.NewLine, xmlLines);
+                var xml = XDocument.Parse(xmlContent);
+
+                var transactions = xml.Descendants("STMTTRN")
+                    .Select(x => new
+                    {
+                        Type = (string)x.Element("TRNTYPE"),
+                        Date = (string)x.Element("DTPOSTED"),
+                        Amount = decimal.Parse((string)x.Element("TRNAMT")),
+                        FitId = (string)x.Element("FITID"),
+                        Memo = (string)x.Element("MEMO")
+                    })
+                    .ToList();
+
+                ViewBag.Message = $"{transactions.Count} transactions parsed successfully!";
+                return View("Send");
+            }
+        }
+    }
+}
--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -9,9 +9,10 @@
 
         public DateTime Date { get; set; }
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
-        public Category Category { get; set; }
+        // to do: add services logic to confirm if user does not want to set Category, default it to "Others"
+        public Category? Category { get; set; }
 
         // to do: add services logic to require PaymentType if this Transaction's Category is a Expense, but not Income.
         public PaymentType? PaymentType { get; set; }
