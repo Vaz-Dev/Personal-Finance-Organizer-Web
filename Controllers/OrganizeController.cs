388642bbdf42e5d8c46bc71963dd187d90cc7969
LucazVaz <lucasvazdev@gmail.com>
2025-07-02 05:15:13 -0300

    Create new category feature

--- Controllers/OrganizeController.cs
+++ Controllers/OrganizeController.cs
@@ -18,9 +18,27 @@ namespace PFO_Web.Controllers
 
         [HttpPost]
 
-        public IActionResult NewCategoryForm(string name, TransactionType type) 
-        { 
-            
+        public IActionResult NewCategoryForm(string categoryName, TransactionType type) 
+        {
+            var data = _xmlService.Load();
+            var newCategory = new Category
+            {
+                Id = data.Categories.Count > 0 ? data.Categories.Max(c => c.Id) + 1 : 1,
+                Name = categoryName,
+                Type = type
+            };
+            if (data.Categories.Any(c => c.Name == newCategory.Name && c.Type == newCategory.Type || c.Id == newCategory.Id))
+            {
+                ViewBag.Danger = "Category with this name and type already exists or ID is not unique.";
+                return View("NewCategory", data);
+            } else
+            {
+                data.Categories.Add(newCategory);
+                _xmlService.Save(data);
+                ViewBag.Success = "Category added successfully!";
+                return View("NewCategory",data);
+            }
+                
         }
 
     }
